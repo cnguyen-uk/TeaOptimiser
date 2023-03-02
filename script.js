2afb1d680a37216be2c190a25979c54ec12b1a88
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2023-03-02 01:10:05 +0000

    Use insertAdjacentHTML

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ form.addEventListener("submit", (event) => {
   // Calculate the required amount of water
   const waterRequired = (people * teacupSize * teacupFill * 0.01);
   const waterRequiredRounded = Math.round(waterRequired);
+  let message = waterRequiredRounded + " ml"
 
   // Display the result and apply the appropriate background color
   resultCell.classList.remove("gray", "red", "yellow", "green");
-  let additionalMsg = '';
 
   if (teapotSize < waterRequired) {
     resultCell.classList.add("gray");
@@ -25,12 +25,12 @@ form.addEventListener("submit", (event) => {
     resultCell.classList.add("red");
   } else if (waterRequired / teapotSize < 0.8) {
     resultCell.classList.add("yellow");
-    additionalMsg = '<br>Fill teapot between ' + Math.round(teapotSize * 0.8) + 'ml and ' + Math.round(teapotSize * 0.9) + 'ml';
+    message += '<br>Fill teapot between ' + Math.round(teapotSize * 0.8) + 'ml and ' + Math.round(teapotSize * 0.9) + 'ml';
   } else {
     resultCell.classList.add("green");
   }
 
-  resultCell.innerText = waterRequiredRounded + " ml" + additionalMsg;
+  resultCell.insertAdjacentHTML("beforeend", message);
 });
 
 // Get all the collapsible headings
